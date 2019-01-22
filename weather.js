22e94d9cb26dc92d18ae8dbb175b95089d33fb12
SangHyeok <tkdgur8377@gmail.com>
2019-01-22 23:49:50 +0900

    1-6

---
+++ weather.js
@@ -0,0 +1,60 @@
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = "c89cbcd0f2a164dc34ac97957032ad7a";
+
+const COORDS = "coords";
+
+function getWeather(lat, lng) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+  )
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      const temperature = json.main.temp;
+      const place = json.name;
+      weather.innerText = `${temperature} @ ${place}`;
+    });
+}
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+
+  const coordsObj = {
+    latitude,
+    longitude
+  };
+  //JavaScript의 키와 값이 똑같은 경우 위같이 할 수 있다.
+  saveCoords(coordsObj);
+  getWeather(latitude, longitude);
+}
+
+function handleGeoError(postion) {
+  console.log(`Can't access Geographic here`);
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
+    askForCoords();
+  } else {
+    const parseCoords = JSON.parse(loadedCoords);
+    getWeather(parseCoords.latitude, parseCoords.longitude);
+  }
+}
+
+function init() {
+  loadCoords();
+}
+
+init();
